a7aecece4f269aeff5e6201fbfe7294089e15885
Josh Eldridge <jeldridge@michiganlabs.com>
2022-05-02 21:50:33 -0400

    Ballot PR fixes

--- app/src/main/java/com/fueledbycaffeine/mivote/ui/voter/ballot/AbsenteeApplicationReceived.kt
+++ app/src/main/java/com/fueledbycaffeine/mivote/ui/voter/ballot/AbsenteeApplicationReceived.kt
@@ -2,32 +2,29 @@ package com.fueledbycaffeine.mivote.ui.voter.ballot
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.fueledbycaffeine.mivote.R
 import com.fueledbycaffeine.mivote.ui.theme.MIVoteTheme
 import io.michiganelections.api.model.VoterRegistration
 
 @Composable
 fun AbsenteeApplicationReceived(registration: VoterRegistration) {
-  Surface(
-    shape = MaterialTheme.shapes.medium,
-    elevation = dimensionResource(R.dimen.elevation_small)
-  ) {
+  Card {
     Column {
       Text(
-        text = "You are registered to vote as absentee!",
+        text = stringResource(R.string.registered_as_absentee),
         style = MaterialTheme.typography.h1,
         modifier = Modifier.padding(dimensionResource(R.dimen.margin_medium))
       )
       Text(
-        text = "Application last received: ${registration.absenteeApplicationReceived ?: "N/A"}",
+        text = stringResource(R.string.application_last_received, registration.absenteeApplicationReceived ?: "N/A"),
         style = MaterialTheme.typography.h1,
         modifier = Modifier.padding(dimensionResource(R.dimen.margin_medium))
       )
--- app/src/main/java/com/fueledbycaffeine/mivote/ui/voter/ballot/NonAbsentee.kt
+++ app/src/main/java/com/fueledbycaffeine/mivote/ui/voter/ballot/NonAbsentee.kt
@@ -2,35 +2,32 @@ package com.fueledbycaffeine.mivote.ui.voter.ballot
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.fueledbycaffeine.mivote.R
 import com.fueledbycaffeine.mivote.ui.PrimaryButton
 import com.fueledbycaffeine.mivote.ui.theme.MIVoteTheme
 
 @Composable
 fun NonAbsentee() {
-  Surface(
-    shape = MaterialTheme.shapes.medium,
-    elevation = dimensionResource(R.dimen.elevation_small)
-  ) {
+  Card {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
       Text(
-        text = "You are not an absentee voter. Click below to view how to register for absentee voting.",
+        text = stringResource(R.string.not_absentee_voter),
         style = MaterialTheme.typography.h1,
         modifier = Modifier.padding(dimensionResource(R.dimen.margin_medium))
       )
       val uriHandler = LocalUriHandler.current
       PrimaryButton(
-        text = "Register Absentee",
+        text = stringResource(R.string.register_absentee),
         onClick = { uriHandler.openUri("https://absentee.michiganelections.io/") },
         modifier = Modifier.padding(dimensionResource(R.dimen.margin_small))
       )
